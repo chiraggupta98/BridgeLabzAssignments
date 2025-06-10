8c26e308cbe11d6c34cdaba4d95354b4cec317ff
Chirag Gupta <chiragggupta007@gmail.com>
2025-06-10 09:14:56 +0530

    Update and rename Add2No.java to AddTwoNo.java

--- Assignment1/AddTwoNo.java
+++ Assignment1/AddTwoNo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Add2No {
+public class AddTwoNo {
     public static void main(String[] args) {
 	    Scanner sc  = new Scanner(System.in);
 	    int a = sc.nextInt();
